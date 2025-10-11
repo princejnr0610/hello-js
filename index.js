7f8ee4efc9c4c2e915c671d82de9e67c68fac1d9
Prince Jr <princejnr0610@gmail.com>
2025-10-11 23:45:15 +0000

    My first commit to git, thanks to BeTechiFied

---
+++ index.js
@@ -0,0 +1,9 @@
+const userinfo = {
+   name : "Prince Junior Asare",
+   age : 30,
+   favProgLang : Javascrip
+
+
+}
+console.log(userinfo.name);
+console.log(userinfo.age);
---
+++ intro.js
@@ -0,0 +1,10 @@
+const userinfo = {
+   name : "Prince Junior Asare",
+   age : 30,
+   favProgLang : "JavaScript"
+
+
+}
+console.log(userinfo.name);
+console.log(userinfo.age);
+console.log(userinfo.favProgLang);
